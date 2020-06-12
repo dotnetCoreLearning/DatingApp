846f3332fc166fba6cd0e51dd0a78f78a4af24bd
dotnetCoreLearning <e.gierscher@vmt-gmbh.de>
2020-06-12 13:52:53 +0200

    End SEction 16

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -34,6 +34,8 @@ import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guards'
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { TimeagoModule } from 'ngx-timeago';
 import {ListsResolver} from "./_resolvers/lists.resolver";
+import {MessagesResolver} from "./_resolvers/messages.resolver";
+import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -52,6 +54,7 @@ export function tokenGetter() {
       MemberDetailComponent,
       MemberEditComponent,
       PhotoEditorComponent,
+      MemberMessagesComponent,
    ],
    imports: [
       BrowserModule,
@@ -83,7 +86,8 @@ export function tokenGetter() {
       MemberListResolver,
       MemberEditResolver,
       PreventUnsavedChanges,
-      ListsResolver
+      ListsResolver,
+     MessagesResolver
    ],
    bootstrap: [
       AppComponent
---
+++ DatingApp.API/Dtos/MessageForCeationDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DatingApp.API.Dtos
+{
+    public class MessageForCeationDto
+    {
+        public int SenderId { get; set; }
+        public int RecipientId { get; set; }
+        public DateTime MessageSent { get; set; }
+        public string Content { get; set; }
+
+        public MessageForCeationDto()
+        {
+            MessageSent = DateTime.Now;
+        }
+    }
+}
